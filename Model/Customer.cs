80815af32019c823e7978cb02682a4a13fd45946
Akshay Upadhya <akshay.upadhya@sonata-software.com>
2022-07-27 15:31:13 +0530

    Add project files.

---
+++ Model/Customer.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppRazor.Model
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        [Required()]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+
+}
